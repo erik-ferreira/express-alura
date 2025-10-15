f3ba21326042d1ab8d15edaa9c3917dc3c72dd7d
Erik Ferreira <erik.souza@vanguardatech.com>
2025-10-14 23:49:55 -0300

    model validator

--- src/models/book.js
+++ src/models/book.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose"
 
-// import { authorSchema } from "./author.js"
-
 const bookSchema = new mongoose.Schema(
   {
     id: { type: String },
@@ -12,13 +10,25 @@ const bookSchema = new mongoose.Schema(
     editora: {
       type: String,
       required: [true, "A editora do livro é obrigatória"],
+      enum: {
+        values: ["Casa do Código", "Alura"],
+        message: "{VALUE} não é uma editora válida",
+      },
     },
     autor: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "autores",
       required: [true, "O autor do livro é obrigatório"],
     },
-    paginas: { type: Number },
+    paginas: {
+      type: Number,
+      // min: [10, "Número mínimo de páginas é 10"],
+      // max: [5000, "Número máximo de páginas é 5000"],
+      validate: {
+        validator: (valor) => valor >= 10 && valor <= 5000,
+        message: "Número de páginas deve estar entre 10 e 5000",
+      },
+    },
   },
   { versionKey: false }
 )
